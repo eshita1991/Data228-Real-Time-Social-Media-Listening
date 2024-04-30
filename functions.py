1f2a0cafe07d4033b1bd7d9a5e2b5ded5c3ee4ac
Madhu Vishwanath <madhuv435@gmail.com>
2024-04-30 12:45:00 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -13,14 +13,14 @@ import re
 
 import nltk
 
-nltk_data_dir = "./resources/nltk_data_dir/"
+"""nltk_data_dir = "./resources/nltk_data_dir/"
 if not os.path.exists(nltk_data_dir):
     os.makedirs(nltk_data_dir, exist_ok=True)
 nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
-nltk.download("stopwords", download_dir=nltk_data_dir)
+nltk.download("stopwords", download_dir=nltk_data_dir)"""
 from nltk.corpus import stopwords
-
+nltk.download("stopwords")
 ## TODO Add Kafka Consumer to get data from Kafka
 
 
