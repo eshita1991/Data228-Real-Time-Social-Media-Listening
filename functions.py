3aa3aca7a111d1f7d1691059a82c464949e72284
Madhu Vishwanath <madhuv435@gmail.com>
2024-04-30 12:45:26 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -20,7 +20,7 @@ nltk.data.path.clear()
 nltk.data.path.append(nltk_data_dir)
 nltk.download("stopwords", download_dir=nltk_data_dir)"""
 from nltk.corpus import stopwords
-nltk.download("stopwords")
+#nltk.download("stopwords")
 ## TODO Add Kafka Consumer to get data from Kafka
 
 
